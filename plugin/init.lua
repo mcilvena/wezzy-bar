a7cd5d605f903ef1e249b2160cbc2d3aea13b068
Troy Mcilvena <troy.mcilvena@gmail.com>
2025-06-13 22:40:09 +1000

    Fix loading errors

--- plugin/init.lua
+++ plugin/init.lua
@@ -1,2 +1,21 @@
 -- Standard plugin entry point following WezTerm conventions
+local wezterm = require 'wezterm'
+
+-- Plugin module path resolution
+local function resolve_plugin_path()
+  for _, plugin in ipairs(wezterm.plugin.list()) do
+    if plugin.url == 'https://github.com/mcilvena/wezzy-bar' then
+      return plugin.plugin_dir
+    end
+  end
+  return nil
+end
+
+local plugin_path = resolve_plugin_path()
+if plugin_path then
+  -- Load the main plugin module
+  package.path = package.path .. ';' .. plugin_path .. '/?.lua'
+  package.path = package.path .. ';' .. plugin_path .. '/?/init.lua'
+end
+
 return require('wezzy-bar')
